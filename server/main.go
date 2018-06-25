130a7d29087dfb4d136b568e2371e7de72b9f06c
zcxzcxczcx <1637960435@qq.com>
2018-06-25 22:05:05 +0800

    new

--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	fmt.Println(1)
 	fmt.Println("Starting the server ...")
 	// 创建 listener
 	listener, err := net.Listen("tcp", "localhost:50000")
@@ -16,6 +17,7 @@ func main() {
 	}
 	// 监听并接受来自客户端的连接
 	for {
+		fmt.Println(2)
 		conn, err := listener.Accept()
 		if err != nil {
 			fmt.Println("Error accepting", err.Error())
@@ -26,9 +28,12 @@ func main() {
 }
 
 func doServerStuff(conn net.Conn) {
+	fmt.Println(3)
 	for {
+		fmt.Println(4)
 		buf := make([]byte, 512)
 		len, err := conn.Read(buf)
+		fmt.Println("len",len)
 		if err != nil {
 			fmt.Println("Error reading", err.Error())
 			return //终止程序
